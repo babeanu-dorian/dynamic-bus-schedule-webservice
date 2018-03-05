18fb99aadb03672b91abfa5c10b1f8695e44d0c5
Ashton Spina <a.d.spina@student.rug.nl>
2018-03-05 17:29:53 +0100

    created basic android app with a webview and spinner with internet permissions configured.  Currently just a template, but can be populated from any Domain

---
+++ app/src/main/java/com/student/spinaashton/dynamicbuswebview/MainActivity.java
@@ -0,0 +1,60 @@
+package com.student.spinaashton.dynamicbuswebview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private Spinner spinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        spinner = (Spinner)findViewById(R.id.spinner1);
+        ArrayAdapter<CharSequence>adapter = ArrayAdapter.createFromResource(this,
+                R.array.spinner_options_array, android.R.layout.simple_spinner_item);
+
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+        WebView myWebView = (WebView) findViewById(R.id.mainViewWebView);
+        //create a webview in the app instead of loading browser to handle loadUrl()
+        myWebView.setWebViewClient(new WebViewClient());
+        //loading page
+        myWebView.loadUrl("http://www.google.com");
+        //enabling javascript
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+
+    }
+
+    //this spinner will be used to choose between browsing routes and stops (which are combinations of routes)
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+        switch (position) {
+            case 0:
+                // Whatever you want to happen when the first item gets selected
+                break;
+            case 1:
+                // Whatever you want to happen when the second item gets selected
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+}
