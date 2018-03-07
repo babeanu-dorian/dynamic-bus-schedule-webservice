1999049d715a727c7d054e97d995078c7a5b35c7
babeanu-dorian <babeanu.dorian@gmail.com>
2018-03-07 01:41:13 +0100

    added sql code for server database and a bash script for database setup

--- Databases/DynamicBusSchedulingServer.sql
+++ Databases/DynamicBusSchedulingServer.sql
@@ -4,7 +4,7 @@ use DynamicBusSchedulingServer;
 
 CREATE TABLE Stations(
 	Id INT UNSIGNED NOT NULL,
-	Key char(16) NOT NULL,      -- used to authenticte the timetable devices
+	AuthenticationKey CHAR(16) NOT NULL,      -- used to authenticte the timetable devices
 	PRIMARY KEY (Id)
 );
 
@@ -13,10 +13,10 @@ CREATE TABLE Routes(
 	PRIMARY KEY (Id)
 );
 
-CREATE TABLE Bus(
+CREATE TABLE Buses(
 	Id INT UNSIGNED NOT NULL,
-	Route INT UNSIGNED,          -- busses can have 0 or 1 routes
-	Key char(16) NOT NULL,       -- used to authenticte the timetable devices
+	Route INT UNSIGNED,                        -- busses can have 0 or 1 routes
+	AuthenticationKey CHAR(16) NOT NULL,       -- used to authenticte the vehicle
 	PRIMARY KEY (Id),
 	FOREIGN KEY (Route) REFERENCES Routes(Id) ON DELETE SET NULL ON UPDATE CASCADE
 );
@@ -24,8 +24,8 @@ CREATE TABLE Bus(
 CREATE TABLE StationsOnRoute(
 	Station INT UNSIGNED NOT NULL,
 	Route INT UNSIGNED NOT NULL,
-	Order INT UNSIGNED NOT NULL,        -- the number of the station on the route
-	CurrentDelay INT NOT NULL           -- negative values are allowed to represent when the bus is ahead of the schedule
+	Location FLOAT(4, 4) UNSIGNED NOT NULL,    -- routeLength / dist(routeStart, stationLocation)
+	CurrentDelay INT NOT NULL,                  -- negative values are allowed to represent when the bus is ahead of the schedule
 	CONSTRAINT PK_StationsOnRoute PRIMARY KEY (Station, Route),
 	FOREIGN KEY (Station) REFERENCES Stations(Id) ON DELETE CASCADE ON UPDATE CASCADE,
 	FOREIGN KEY (Route) REFERENCES Routes(Id) ON DELETE CASCADE ON UPDATE CASCADE
@@ -34,17 +34,21 @@ CREATE TABLE StationsOnRoute(
 CREATE TABLE ExpectedTimes(
 	Station INT UNSIGNED NOT NULL,
 	Route INT UNSIGNED NOT NULL,
-	Hour TINYINT UNSIGNED NOT NULL,     -- 1st hour of the time interval
+	Hour TINYINT UNSIGNED NOT NULL,            -- 1st hour of the time interval
 	Month TINYINT UNSIGNED NOT NULL,
-	Duration INT NOT NULL,              -- time to reach station from previous station
+	Duration INT NOT NULL,                     -- time to reach station from previous station
 	CONSTRAINT PK_StationsOnRoute PRIMARY KEY (Station, Route, Hour, Month),
 	FOREIGN KEY (Station) REFERENCES Stations(Id) ON DELETE CASCADE ON UPDATE CASCADE,
 	FOREIGN KEY (Route) REFERENCES Routes(Id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE ActualTimes(
-	
+CREATE TABLE RecordedTimes(
+	Id INT UNSIGNED NOT NULL AUTO_INCREMENT,
 	Bus INT UNSIGNED NOT NULL,
 	Route INT UNSIGNED NOT NULL,
-
+	Progress FLOAT(4, 4) UNSIGNED NOT NULL,
+	TimeOfRecord DATETIME NOT NULL,
+	PRIMARY KEY (Id),
+	FOREIGN KEY (Bus) REFERENCES Buses(Id) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (Route) REFERENCES Routes(Id) ON DELETE CASCADE ON UPDATE CASCADE
 );
