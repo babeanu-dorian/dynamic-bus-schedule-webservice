d3f9970ed8022dff1475a20329e9b1f582f9d0c3
A D Spina <a.d.spina@student.rug.nl>
2018-04-02 19:27:21 +0200

    Update MainActivity.java
    Modified what is loaded by the webview.  We should modify this when we know the IP in the demo and recompile the webapp

--- dynamicScheduleApp/app/src/main/java/com/student/spinaashton/dynamicbuswebview/MainActivity.java
+++ dynamicScheduleApp/app/src/main/java/com/student/spinaashton/dynamicbuswebview/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         //create a webview in the app instead of loading browser to handle loadUrl()
         myWebView.setWebViewClient(new WebViewClient());
         //loading page
-        myWebView.loadUrl("http://www.google.com");
+        myWebView.loadUrl("http://www.192.168.0.104:3000/");
         //enabling javascript
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
