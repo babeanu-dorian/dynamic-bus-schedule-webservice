9723d61c330d618575ea2d5754b4ab486f862238
Daffurn <alexlewis-@hotmail.co.uk>
2018-04-02 14:32:37 +0200

    homepage fixes and stop mock object

--- MockObjects/mock_stop.js
+++ MockObjects/mock_stop.js
@@ -1,12 +1,50 @@
-module.exports = function(key, id, serverList) {
+var request = require('request');
+
+module.exports = function(key, id, routeList, url) {
 
 	return {
 		key:key,
 		id:id,
-		serverList:serverList,
+		mapRouteServer:{},
+		routeList:routeList,
 		schedule:[],
+		url:url,
+		initalize: function() {
+			request.get(this.url + "/appData", (error, response, body) => {
+				if(error) {
+					console.log(error);
+				} else {
+					this.mapRouteServer = body.mapRouteServer;
+				}
+			});
+		}
 		update: function() {
-			//Get Schedule from socket, update it to this.schedule and sort
+			schedule = [];
+			for(int i = 0 ; i < routeList.length ; i++) {
+				request.post(
+				{
+					url:this.mapRouteServer["r" + routeList[i]] + "/routeData",
+					body:JSON.stringify({
+						route:routeList[i],
+						station:this.id,
+					})
+				},
+				function(error, response, body){
+					if (error) {
+						console.log(error); // TODO: select another server from serverList in case of error
+					} else if (!body.schedule) {
+						this.mapRouteServer["r" + routeList[i]] = body.serverOfRoute;
+						this.update();
+					} else {
+						schedule.concat(body.schedule);
+					}
+				})
+			}
+		}
+		display: function() {
+			schedule.sort(function(a , b) {
+				return a.arrivalTime - b.arrivalTime;
+			})
 		}
 	}
 }
